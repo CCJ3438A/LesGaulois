4710b5f73e7b486c91081a70a0adb28b13c6e93f
joyce <joyce@joyce-VivoBook-ASUSLaptop-X515DA-M515DA>
2022-10-19 14:35:51 +0200

    ILU1 TP2.c

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,36 +3,41 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
-	
+
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
 	}
-	
+
 	public String getNom() {
-		return nom;
+		return this.nom;
 	}
-	
+
 	public void parler(String texte) {
-		System.out.println (prendreParole() + "« " + texte + "»");
+		System.out.println(this.prendreParole() + "« " + texte + "»");
 	}
 
 	private String prendreParole() {
-		return "Le romain " + nom + " : ";
+		return "Le romain " + this.nom + " : ";
 	}
-	
+
 	public void recevoirCoup(int forceCoup) {
-		force -= forceCoup;
-		
-		if (force > 0) {
-			parler("Aïe");
+		assert this.force > 0;
+		int postCondForce = this.force;
+		this.force -= forceCoup;
+
+		if (this.force > 0) {
+			this.parler("Aïe");
 		} else {
-			parler("J'abandonne...");
+			this.parler("J'abandonne...");
 		}
+		assert this.force < postCondForce;
 	}
 
 	public static void main(String[] args) {
-		Romain soldatouris2 = new Romain("soldatouris2",102);
+		Romain soldatouris2 = new Romain("soldatouris2", 6);
+		assert soldatouris2.force > 0;
+
 		soldatouris2.prendreParole();
 		soldatouris2.parler("EN GAAAAAAAAAAAAAAAAARRRRDE !");
 		soldatouris2.recevoirCoup(100);
